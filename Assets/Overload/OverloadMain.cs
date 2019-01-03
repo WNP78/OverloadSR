009cf5fb52fb530820b26e2a1fe5dcf48580983d
WNP78 <tgbyhnujmrfvedcwsx@gmail.com>
2019-01-03 12:02:35 +0000

    Added GetXML and SaveXML methods (mostly from old projects, rewritten)

--- Assets/Overload/OverloadMain.cs
+++ Assets/Overload/OverloadMain.cs
@@ -16,12 +16,14 @@ namespace WNP78.Overload
         static Type DesignerScript;
         static Type CraftBuilder;
         static string DialogXML;
+        static ConstructorInfo PartDataConstructor;
         public static void Init()
         {
             ServiceProvider.Instance.Game.SceneManager.SceneLoaded += OnSceneLoaded;
             CraftBuilder = ReflectionUtils.GetType("Assets.Scripts.Craft.CraftBuilder");
             DesignerScript = ReflectionUtils.GetType("Assets.Scripts.Design.DesignerScript");
             DialogXML = ServiceProvider.Instance.ResourceLoader.LoadAsset<TextAsset>("Assets/Dialog.xml").text;
+            PartDataConstructor = typeof(PartData).GetConstructor(new Type[] { typeof(XElement), typeof(int), typeof(PartType) });
         }
         static void OnSceneLoaded(object sender, ModApi.Scenes.Events.SceneEventArgs e)
         {
@@ -44,6 +46,43 @@ namespace WNP78.Overload
         static void SelectedPartChanged(IPartScript oldPart, IPartScript newPart)
         {
             
+        }
+
+        static IPartScript part;
+        static XElement backupXml;
+        public static XElement GetXML()
+        {
+            part = Designer.SelectedPart;
+            var res = part.Data.GenerateXml(part.Transform, false);
+            backupXml = XElement.Parse(res.ToString(SaveOptions.DisableFormatting));
+            return res;
+        }
+        public static void SaveXML(XElement xml)
+        {
+            var partData = part.Data;
+            var partType = GetPartType(Utilities.GetStringAttribute(xml, "partType", ""));
+            if (partType == null)
+            {
+                ServiceProvider.Instance.Game.Designer.DesignerUi.ShowMessage("partType not found! Aborting.");
+            }
+            try
+            {
+                PartDataConstructor.Invoke(partData, new object[] { xml, Designer.CraftScript.Data.XmlVersion, partType });
+            }
+            catch (Exception e)
+            {
+                Debug.LogError(e);
+                ServiceProvider.Instance.Game.Designer.DesignerUi.ShowMessage("XML Load error (check console for details). Reverting");
+                PartDataConstructor.Invoke(partData, new object[] { backupXml, Designer.CraftScript.Data.XmlVersion, partType });
+            }
+
+            UnityEngine.Object.Destroy(part.GameObject);
+            CraftBuilder.CallS("CreatePartGameObjects", new PartData[1] { partData }, Designer.CraftScript);
+            Designer.SelectPart(partData.PartScript, null, true);
+        }
+        static PartType GetPartType(string name)
+        {
+            return (PartType)ServiceProvider.Instance.Game.GetP("PartTypeList").GetP("GetPartType", name);
         }
     }
 }
