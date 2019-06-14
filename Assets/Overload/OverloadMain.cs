96bf5d533cb48fc9585b088e5880121ab03f9f89
WNP78 <wnp78g@gmail.com>
2019-06-14 19:30:20 +0100

    Convert OverloadMain to a GameMod class, static to singleton.

--- Assets/Overload/OverloadMain.cs
+++ Assets/Overload/OverloadMain.cs
@@ -11,51 +11,55 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using ModApi.Craft.Parts;
+using ModApi.Mods;
+using ModApi.Common;
+using Assets.Packages.DevConsole;
 
 namespace WNP78.Overload
 {
-    public static class OverloadMain
+    public class OverloadMain : GameMod
     {
-        public static bool Active
+        public static OverloadMain Instance { get; } = GetModInstance<OverloadMain>();
+        public bool Active
         {
             get { return Designer != null; }
         }
-        static IDesigner Designer;
-        public static Type CraftBuilder;
-        public static Type UiUtilities;
-        public static Type Symmmetry;
-        public static MethodInfo CreateXmlLayoutFromXml;
-        public static string DialogXML;
-        public static string ButtonXML;
-        static ConstructorInfo PartDataConstructor;
-        static GameObject OverloadButtonObject;
+        IDesigner Designer;
+        public Type CraftBuilder;
+        public Type UiUtilities;
+        public Type Symmmetry;
+        public MethodInfo CreateXmlLayoutFromXml;
+        public string DialogXML;
+        public string ButtonXML;
+        ConstructorInfo PartDataConstructor;
+        GameObject OverloadButtonObject;
 
-        static readonly Regex Regex1 = new Regex("(.*?)<(\\w+) ([^>/]+)(\\/?>)"); // selects element bodies
-        static readonly Regex Regex2 = new Regex("\\s*(\\w+=\"[^\"]*\")\\s*"); // separates out attributes
+        readonly Regex Regex1 = new Regex("(.*?)<(\\w+) ([^>/]+)(\\/?>)"); // selects element bodies
+        readonly Regex Regex2 = new Regex("\\s*(\\w+=\"[^\"]*\")\\s*"); // separates out attributes
 
-        public static void Init()
+        protected override void OnModInitialized()
         {
-            ServiceProvider.Instance.Game.SceneManager.SceneLoaded += OnSceneLoaded;
+            base.OnModInitialized();
+            Game.Instance.SceneManager.SceneLoaded += OnSceneLoaded;
             CraftBuilder = ReflectionUtils.GetType("Assets.Scripts.Craft.CraftBuilder");
             UiUtilities = ReflectionUtils.GetType("Assets.Scripts.Ui.UiUtilities");
             Symmmetry = ReflectionUtils.GetType("Assets.Scripts.Design.Symmetry");
-            DialogXML = ServiceProvider.Instance.ResourceLoader.LoadAsset<TextAsset>("Assets/Dialog.xml").text;
-            ButtonXML = ServiceProvider.Instance.ResourceLoader.LoadAsset<TextAsset>("Assets/Button.xml").text;
+            DialogXML = ResourceLoader.LoadAsset<TextAsset>("Assets/Dialog.xml").text;
+            ButtonXML = ResourceLoader.LoadAsset<TextAsset>("Assets/Button.xml").text;
             CreateXmlLayoutFromXml = UiUtilities.GetMethods(ReflectionUtils.allBindingFlags).First(m => m.Name == "CreateXmlLayoutFromXml" && m.GetParameters().Length == 4);
             PartDataConstructor = typeof(PartData).GetConstructor(new Type[] { typeof(XElement), typeof(int), typeof(PartType) });
-            ServiceProvider.Instance.DevConsole.RegisterCommand<string>("LoadDialogXmlFromPath", LoadDialogXmlFromPath);
-            
+            DevConsoleApi.RegisterCommand<string>("LoadDialogXmlFromPath", LoadDialogXmlFromPath);
         }
-        static void LoadDialogXmlFromPath(string path)
+        void LoadDialogXmlFromPath(string path)
         {
             if (!Active) { return; }
             DialogXML = System.IO.File.ReadAllText(path);
         }
-        static void OnSceneLoaded(object sender, ModApi.Scenes.Events.SceneEventArgs e)
+        void OnSceneLoaded(object sender, ModApi.Scenes.Events.SceneEventArgs e)
         {
             if (e.Scene == ModApi.Scenes.SceneNames.Designer)
             {
-                Designer = ServiceProvider.Instance.Game.Designer;
+                Designer = Game.Instance.Designer;
                 var flyout = Designer.DesignerUi.Flyouts.PartProperties;
                 Designer.SelectedPartChanged += SelectedPartChanged;
 
@@ -72,13 +76,13 @@ namespace WNP78.Overload
                 Designer = null;
             }
         }
-        static void OnButtonLayoutRebuilt(IXmlLayoutController xmlLayoutController)
+        void OnButtonLayoutRebuilt(IXmlLayoutController xmlLayoutController)
         {
             OverloadButtonObject = xmlLayoutController.XmlLayout.GetElementById<RectTransform>("overload-button").gameObject;
             OverloadButtonObject.GetComponent<Button>().onClick.AddListener(EditXmlButtonClicked);
             SelectedPartChanged(null, Designer.SelectedPart);
         }
-        static void SelectedPartChanged(IPartScript oldPart, IPartScript newPart)
+        void SelectedPartChanged(IPartScript oldPart, IPartScript newPart)
         {
             try
             {
@@ -86,14 +90,14 @@ namespace WNP78.Overload
             }
             catch (NullReferenceException) { }
         }
-        static void EditXmlButtonClicked()
+        void EditXmlButtonClicked()
         {
             OverloadXmlEditDialogScript.Create(Designer.DesignerUi.Transform, PrettifyXml(GetXML().ToString()), SaveXML);
         }
 
-        static IPartScript part;
-        static XElement backupXml;
-        public static XElement GetXML()
+        IPartScript part;
+        XElement backupXml;
+        public XElement GetXML()
         {
             part = Designer.SelectedPart;
             var res = part.Data.GenerateXml(part.CraftScript.Transform, false);
@@ -101,7 +105,7 @@ namespace WNP78.Overload
             backupXml = XElement.Parse(res.ToString(SaveOptions.DisableFormatting));
             return res;
         }
-        public static void SaveXML(XElement xml)
+        public void SaveXML(XElement xml)
         {
             var partData = part.Data;
             var oldConns = partData.PartConnections;
@@ -109,7 +113,7 @@ namespace WNP78.Overload
             var partType = GetPartType(Utilities.GetStringAttribute(xml, "partType", ""));
             if (partType == null)
             {
-                ServiceProvider.Instance.Game.Designer.DesignerUi.ShowMessage("partType not found! Aborting.");
+                Game.Instance.Designer.DesignerUi.ShowMessage("partType not found! Aborting.");
             }
             try
             {
@@ -118,7 +122,7 @@ namespace WNP78.Overload
             catch (Exception e)
             {
                 Debug.LogError(e);
-                ServiceProvider.Instance.Game.Designer.DesignerUi.ShowMessage("XML Load error (check console for details). Reverting");
+                Game.Instance.Designer.DesignerUi.ShowMessage("XML Load error (check console for details). Reverting");
                 PartDataConstructor.Invoke(partData, new object[] { backupXml, Designer.CraftScript.Data.XmlVersion, partType });
             }
             partData.SetP("PartConnections", oldConns);
@@ -136,11 +140,11 @@ namespace WNP78.Overload
 
             Designer.CraftScript.RaiseDesignerCraftStructureChangedEvent();
         }
-        static PartType GetPartType(string name)
+        PartType GetPartType(string name)
         {
-            return (PartType)ServiceProvider.Instance.Game.GetP("PartTypes").Call("GetPartType", name);
+            return (PartType)Game.Instance.GetP("PartTypes").Call("GetPartType", name);
         }
-        static string PrettifyXml(string xmlIn)
+        string PrettifyXml(string xmlIn)
         {
             return Regex1.Replace(xmlIn, match =>
             {
